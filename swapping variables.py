70c26737a07ed3971f5fc73f98200a77a268c721
janvipatil585-collab <janvipatil585@gmail.com>
2026-01-07 17:59:46 +0530

    python program to swap two variables without using a third variable

---
+++ swapping variables.py
@@ -0,0 +1,13 @@
+def swap(a, b):
+    return b,a
+
+x=int(input('enter your number:'))
+y=int(input('enter your number:'))
+
+x,y=swap(x,y)
+
+print('After swapping:')
+print('x=',x)
+print('y=',y)
+
+  
