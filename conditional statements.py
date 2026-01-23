b7a14f36bce47d7cb2e6e09c347afba5403d13d8
janvipatil585-collab <janvipatil585@gmail.com>
2026-01-23 21:04:47 +0530

    if-elif-else conditional statements used in python

---
+++ conditional statements.py
@@ -0,0 +1,10 @@
+state=input("how you are feeling:")
+
+if(state=="Hungry"):
+    print("order a pizza")
+elif(state=="TIRED"):
+    print("take a nap")
+elif(state=="bored"):
+    print("watch a movie")
+else:
+    print("go for a walk")
